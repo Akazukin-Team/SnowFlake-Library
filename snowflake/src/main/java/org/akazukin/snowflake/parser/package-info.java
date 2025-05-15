47cf9614bf59e3c367c881c5ab7bc82e15fd4dc9
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2025-05-16 06:18:42 +0900

    Add Snowflake parser utility package.
    Introduce `SnowFlakeParser` to parse Snowflake IDs.
    Includes configuration-based structure and result encapsulation.

---
+++ snowflake/src/main/java/org/akazukin/snowflake/parser/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package provides utility for parsing the Snowflake.
+ *
+ * <p>
+ * The classes in this package are designed to facilitate the parsing of Snowflake objects,
+ * ensuring efficient and accurate processing. Users can leverage these utilities to handle
+ * data transformations specific to the Snowflake framework.
+ */
+package org.akazukin.snowflake.parser;
